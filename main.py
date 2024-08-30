cb4d405f58021e06c49c5f914de7fd22eeec0730
ChinHongTan <52919589+ChinHongTan@users.noreply.github.com>
2024-08-30 15:27:15 +0800

    chore: Update main.py with FastAPI integration and asyncio event loop

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
 import logging
 import config
+import asyncio
 from config import COMMANDS_FOLDER
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+import uvicorn
 
 import discord
 from dotenv import load_dotenv
@@ -14,13 +19,13 @@ logger = logging.getLogger(__name__)
 # Init bot
 intents = discord.Intents.default()
 intents.message_content = True
-bot = discord.Bot(intents=intents)
+loop = asyncio.get_event_loop()
+bot = discord.Bot(intents=intents, loop=loop)
 
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is running !")
-
 
 for filepath in COMMANDS_FOLDER.rglob('*.py'):
     relative_path = filepath.relative_to(COMMANDS_FOLDER).with_suffix('')
@@ -29,5 +34,21 @@ for filepath in COMMANDS_FOLDER.rglob('*.py'):
     logging.info(f'Loading {module_name}')
     bot.load_extension(module_name)
 
+app = FastAPI()
+config = uvicorn.Config(app, loop="asyncio")
+server = uvicorn.Server(config)
 
-bot.run(BOT_TOKEN)
+@app.get("/")
+async def ping():
+    return {"message": "pong"}
+
+async def start() -> None:
+    await asyncio.gather(bot.start(BOT_TOKEN), server.serve())
+
+try:
+    loop.run_until_complete(start())
+finally:
+    if not bot.is_closed():
+        loop.run_until_complete(bot.close())
+    if server.started:
+        loop.run_until_complete(server.shutdown())
