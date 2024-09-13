a606be5bc70d054c02bc57bae88e20cab3afc9bf
Shewiiii <akiempj123@gmail.com>
2024-09-13 21:19:52 +0200

    Add /previous command

---
+++ commands/vocal/previous.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+from bot.vocal import *
+
+
+class Previous(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    @commands.slash_command(
+        name='previous',
+        description='Play the previous track.'
+    )
+    async def previous(self, ctx: discord.ApplicationContext):
+        guild_id = ctx.guild.id
+        session = server_sessions.get(guild_id)
+
+        if session is None:
+            await ctx.respond('No active sessions!')
+            return
+
+        if not session.stack_previous:
+            await ctx.respond("No tracks played previously!")
+            return
+
+        await ctx.respond('Playing the previous track!')
+        await session.play_previous(ctx)
+
+
+def setup(bot):
+    bot.add_cog(Previous(bot))
