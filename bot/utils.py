005c194cfce2164278ce38d632adfb4e0a27c35e
Shewiiii <akiempj123@gmail.com>
2024-10-02 22:39:37 +0200

    Ignore folders during cache cleanup

--- bot/utils.py
+++ bot/utils.py
@@ -221,7 +221,10 @@ def cleanup_cache() -> None:
 
     This function removes files that exceed the cache size limit and deletes expired files.
     """
-    files = sorted(TEMP_FOLDER.glob('*'), key=os.path.getmtime)
+    files = sorted(
+        [f for f in TEMP_FOLDER.rglob('*') if f.is_file()], 
+        key=os.path.getmtime
+    )
 
     # Remove files that exceed the cache size limit
     while len(files) > CACHE_SIZE:
