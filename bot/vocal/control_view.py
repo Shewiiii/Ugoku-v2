de25abb84bd7a00888c26d22eb8deef4c488c07a
Shewiiii <akiempj123@gmail.com>
2024-10-30 14:31:22 +0100

    Split ControlView class into a dedicated file

---
+++ bot/vocal/control_view.py
@@ -0,0 +1,80 @@
+
+import discord
+
+
+# VIEW (for buttons under the "now playing" embed)
+class controlView(discord.ui.View):
+    def __init__(
+        self,
+        bot: discord.bot,
+        ctx: discord.ApplicationContext,
+        voice_client: discord.voice_client
+    ) -> None:
+        super().__init__(timeout=None)
+        self.bot = bot
+        self.ctx = ctx
+        self.voice_client = voice_client
+
+    @discord.ui.button(
+        label="Pause/Resume",
+        style=discord.ButtonStyle.secondary,
+    )
+    async def pause_button_callback(
+        self,
+        button: discord.ui.Button,
+        interaction: discord.Interaction
+    ) -> None:
+        # To avoid "interaction failed message"
+        await interaction.response.defer()
+
+        # Pause if audio is playing
+        if self.voice_client.is_playing():
+            pause_cog = self.bot.get_cog('Pause')
+            await pause_cog.execute_pause(self.ctx)
+
+        # Resume if audio is paused
+        else:
+            resume_cog = self.bot.get_cog('Resume')
+            await resume_cog.execute_resume(self.ctx)
+
+    @discord.ui.button(
+        label="Play previous",
+        style=discord.ButtonStyle.secondary,
+    )
+    async def previous_button_callback(
+        self,
+        button: discord.ui.Button,
+        interaction: discord.Interaction
+    ) -> None:
+        await interaction.response.defer()
+
+        previous_cog = self.bot.get_cog('Previous')
+        await previous_cog.execute_previous(self.ctx)
+
+    @discord.ui.button(
+        label="Skip",
+        style=discord.ButtonStyle.secondary,
+    )
+    async def skip_button_callback(
+        self,
+        button: discord.ui.Button,
+        interaction: discord.Interaction
+    ) -> None:
+        await interaction.response.defer()
+
+        skip_cog = self.bot.get_cog('Skip')
+        await skip_cog.execute_skip(self.ctx)
+
+    @discord.ui.button(
+        label="Loop song",
+        style=discord.ButtonStyle.secondary,
+    )
+    async def loop_button_callback(
+        self,
+        button: discord.ui.Button,
+        interaction: discord.Interaction
+    ) -> None:
+        await interaction.response.defer()
+
+        loop_cog = self.bot.get_cog('Loop')
+        await loop_cog.execute_loop(self.ctx, 'Song')
--- bot/vocal/server_session.py
+++ bot/vocal/server_session.py
@@ -9,6 +9,7 @@ from librespot.audio import AbsChunkedInputStream
 
 from api.update_active_servers import update_active_servers
 from bot.vocal.queue_view import QueueView
+from bot.vocal.control_view import controlView
 from bot.vocal.types import QueueItem, TrackInfo, LoopMode, SimplifiedTrackInfo
 from config import AUTO_LEAVE_DURATION
 
@@ -132,82 +133,6 @@ class ServerSession:
             message = ''
 
             # VIEW (buttons)
-            class controlView(discord.ui.View):
-                def __init__(
-                    self,
-                    bot: discord.bot,
-                    ctx: discord.ApplicationContext,
-                    voice_client: discord.voice_client
-                ) -> None:
-                    super().__init__(timeout=None)
-                    self.bot = bot
-                    self.ctx = ctx
-                    self.voice_client = voice_client
-
-                @discord.ui.button(
-                    label="Pause/Resume",
-                    style=discord.ButtonStyle.secondary,
-                )
-                async def pause_button_callback(
-                    self,
-                    button: discord.ui.Button,
-                    interaction: discord.Interaction
-                ) -> None:
-                    # To avoid "interaction failed message"
-                    await interaction.response.defer()
-
-                    # Pause if audio is playing
-                    if self.voice_client.is_playing():
-                        pause_cog = self.bot.get_cog('Pause')
-                        await pause_cog.execute_pause(ctx)
-
-                    # Resume if audio is paused
-                    else:
-                        resume_cog = self.bot.get_cog('Resume')
-                        await resume_cog.execute_resume(ctx)
-
-                @discord.ui.button(
-                    label="Play previous",
-                    style=discord.ButtonStyle.secondary,
-                )
-                async def previous_button_callback(
-                    self,
-                    button: discord.ui.Button,
-                    interaction: discord.Interaction
-                ) -> None:
-                    await interaction.response.defer()
-
-                    previous_cog = self.bot.get_cog('Previous')
-                    await previous_cog.execute_previous(ctx)
-
-                @discord.ui.button(
-                    label="Skip",
-                    style=discord.ButtonStyle.secondary,
-                )
-                async def skip_button_callback(
-                    self,
-                    button: discord.ui.Button,
-                    interaction: discord.Interaction
-                ) -> None:
-                    await interaction.response.defer()
-
-                    skip_cog = self.bot.get_cog('Skip')
-                    await skip_cog.execute_skip(ctx)
-
-                @discord.ui.button(
-                    label="Loop song",
-                    style=discord.ButtonStyle.secondary,
-                )
-                async def loop_button_callback(
-                    self,
-                    button: discord.ui.Button,
-                    interaction: discord.Interaction
-                ) -> None:
-                    await interaction.response.defer()
-
-                    loop_cog = self.bot.get_cog('Loop')
-                    await loop_cog.execute_loop(ctx, 'Song')
-
             view = controlView(self.bot, ctx, self.voice_client)
 
         else:
